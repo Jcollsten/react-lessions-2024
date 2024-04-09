863bd9d85ed3a4f9185723f8b17ced62b9757362
Jonas <jonas.collsten@gmail.com>
2024-04-09 23:04:58 +0200

    Assignments 1-6 from lession 4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { Counter, UserProfile } from './components/lession3';
 import { Timer } from './components/lession3';
-import { TraditionalGreeting, Greet, UserProfile2 } from './components/lession4';
+import { TraditionalGreeting, Greet, UserProfile2, Card, Counter2, TodoList } from './components/lession4';
+
+// import { BonsaiTreeImage } from './pictures';
 
 function App() {
   return (
@@ -37,6 +39,25 @@ function App() {
           age={28}
           location='Stockholm, Sweden'
         />
+      </div>
+      <div className='userProfile'>
+        <Card>
+          <h4>Assignment 1.3, lession 4</h4>
+          <h2>A Bonsai Tree</h2>
+          <p>lorem ipsum Bonsai Tree 78 years old, </p>
+          {/* <img
+            src={BonsaiTreeImage}
+            alt='tree'
+          ></img> */}
+        </Card>
+      </div>
+      <div className='userProfile'>
+        <h4>Assignment 1.4 & 1.5, lession 4</h4>
+        <Counter2 />
+      </div>
+      <div className='userProfile'>
+        <h4>Assignment 1.6, lession 4</h4>
+        <TodoList></TodoList>
       </div>
     </div>
   );
--- src/components/lession4.jsx
+++ src/components/lession4.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect, useReducer } from 'react';
 
 export function TraditionalGreeting(props) {
   return (
@@ -10,12 +10,120 @@ export function TraditionalGreeting(props) {
 
 export const Greet = (props) => <h3>{`Welcome again ${props.name}, but with a Arrow-Fuction `}</h3>;
 
-export function UserProfile2({ name, age, location }) {
+export function UserProfile2({ name = 'Anonym', age, location }) {
   return (
     <>
       <h2>Welcome {name}</h2>
       <h2>Age: {age}</h2>
       <h2>Location: {location}</h2>
     </>
+  );
+}
+
+export function Card({ children }) {
+  return <div className='Card'>{children}</div>;
+}
+
+export function Counter2() {
+  const [count, setCount] = useState(0);
+
+  function TitleUpdater() {
+    useEffect(() => {
+      if (count === 0) document.title = 'React App';
+      else document.title = `User has clicked ${count} times`;
+    });
+  }
+
+  function handleClickDecrease() {
+    if (count > 0) setCount((c) => c - 1);
+  }
+  function handleClickIncrease() {
+    setCount((c) => c + 1);
+  }
+  return (
+    <>
+      <TitleUpdater />
+      <p>You have clicked {count} times</p>
+      <button
+        className='counter-btn'
+        onClick={() => handleClickIncrease()}
+      >
+        Click me to increse
+      </button>
+      <button
+        className='counter-btn'
+        onClick={() => handleClickDecrease()}
+      >
+        Click me reduce
+      </button>
+    </>
+  );
+}
+
+const initialState = {
+  tasks: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return {
+        tasks: [...state.tasks, action.payload],
+      };
+    case 'REMOVE_TASK':
+      return {
+        tasks: state.tasks.filter((task) => task !== action.payload),
+      };
+    default:
+      return state;
+  }
+}
+
+export function TodoList() {
+  const [taskInput, setTaskInput] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleAddTask = () => {
+    if (taskInput.trim() !== '') {
+      dispatch({ type: 'ADD_TASK', payload: taskInput });
+      setTaskInput('');
+    }
+  };
+
+  const handleRemoveTask = (task) => {
+    dispatch({ type: 'REMOVE_TASK', payload: task });
+  };
+
+  return (
+    <div className='list-container'>
+      <input
+        type='text'
+        value={taskInput}
+        onChange={(e) => setTaskInput(e.target.value)}
+        placeholder='Add a task'
+      />
+      <button
+        className='counter-btn'
+        onClick={handleAddTask}
+      >
+        Add Task
+      </button>
+      <ul>
+        {state.tasks.map((task, index) => (
+          <li
+            className='list'
+            key={index}
+          >
+            {task}
+            <button
+              className='counter-btn'
+              onClick={() => handleRemoveTask(task)}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
